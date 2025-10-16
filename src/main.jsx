51098dd4f0c6634298ecd4aa6b895753cf830bf2
SDLIEB <SOL@ROBERTS-LIEB.COM>
2025-10-16 13:31:59 -0400

    Wrapping Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Your Tailwind styles
+import './index.css';
+// 👈 ADD THIS IMPORT
+import { BrowserRouter } from 'react-router-dom'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* 👈 WRAP <App /> WITH <BrowserRouter> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
