74a646c9d1416328e07cfdd2f06628f8f44a0d22
SDLIEB <SOL@ROBERTS-LIEB.COM>
2025-10-18 07:33:24 -0400

    fix utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,8 +1,15 @@
 // src/lib/utils.js
 
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+/**
+ * Utility function to conditionally combine CSS classes and intelligently merge
+ * Tailwind CSS utility classes to avoid conflicts.
+ * @param {ClassValue[]} inputs - List of class names or conditional class objects.
+ * @returns {string} - The merged class string.
+ */
+export function cn(...inputs: ClassValue[]) {
+    // twMerge handles merging Tailwind classes intelligently
+    return twMerge(clsx(inputs));
 }
