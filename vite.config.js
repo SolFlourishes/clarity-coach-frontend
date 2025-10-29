977350a0dd6455dda7c50e52a0fde189f1d7fca6
SDLIEB <SOL@ROBERTS-LIEB.COM>
2025-10-29 07:06:57 -0400

    feat: reverted vite base to absolute /apps/clarity/ for final deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Change from '/apps/clarity/' to './' (relative path)
-  base: './', 
+  // Set the absolute path for asset resolution 
+  base: '/apps/clarity/', 
 })
